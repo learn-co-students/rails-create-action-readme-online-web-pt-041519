c1e43191525b9eb2211b13655676f963845486d2
kimpham54 <kimpham54@gmail.com>
2019-10-01 11:27:39 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
-
+def create
+  @post = Post.new
+  @post.title = params[:title]
+  @post.description = params[:description]
+  @post.save
+  redirect_to post_path(@post)
+end
 end
